188eb618460960e17f47bed963e2e29e701e2837
Jean Yang <bdlas321@gmail.com>
2016-04-12 22:55:40 +0800

    store checkbox checked info

--- src/components/TrackSelector.js
+++ src/components/TrackSelector.js
@@ -3,24 +3,48 @@ import React, { Component } from 'react';
 export default class TrackSelector extends Component {
   constructor(props){
     super(props);
+    const contentObject = JSON.parse(props.content);
+    const tracks = contentObject.project.wavetrack.length;
+    const checkedBoxes = [];
+    for(var i = 0; i<tracks; i++){
+      checkedBoxes.push( (i%2===0)? true : false );
+    }
+    this.state = {
+      checkedBoxes
+    };
+  }
+
+  onCheckboxChange(index){
+    const changedCheckedBoxes = this.state.checkedBoxes.slice(0);
+    changedCheckedBoxes[index] = !changedCheckedBoxes[index];
+    this.setState({
+      checkedBoxes: changedCheckedBoxes
+    });
   }
 
   renderTracks(content){
+    let self = this;
     let contentObject = JSON.parse(content);
     return contentObject.project.wavetrack.map( (track, i) => {
-        let checked = (i%2===0)? true : false;
-        return (
-          <tr key={`track${i}`}>
-            <td><input type="checkbox" name="selectedTracks" value={i} defaultChecked={checked} /></td>
-            <td>{track["$"].name}</td>
-            <td>{(track.waveclip)? track.waveclip.length:'unknown'}</td>
-            <td>{track["$"].channel}</td>
-            <td>{track["$"].linked}</td>
-            <td>{track["$"].rate}</td>
-            <td>{track["$"].mute}</td>
-            <td>{track["$"].solo}</td>
-          </tr>
-        );
+      return (
+        <tr key={`track${i}`}>
+          <td>
+            <input type="checkbox"
+              name="selectedTracks"
+              value={i}
+              checked={self.state.checkedBoxes[i]}
+              onChange={self.onCheckboxChange.bind(this, i)}
+            />
+          </td>
+          <td>{track["$"].name}</td>
+          <td>{(track.waveclip)? track.waveclip.length:'unknown'}</td>
+          <td>{track["$"].channel}</td>
+          <td>{track["$"].linked}</td>
+          <td>{track["$"].rate}</td>
+          <td>{track["$"].mute}</td>
+          <td>{track["$"].solo}</td>
+        </tr>
+      );
     });
   }
 
