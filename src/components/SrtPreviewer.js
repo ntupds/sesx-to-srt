4379282af655a08eee9a5945b25199af1196422e
Jean Yang <bdlas321@gmail.com>
2016-04-13 04:22:28 +0800

    button onClick: export srt

--- src/components/SrtPreviewer.js
+++ src/components/SrtPreviewer.js
@@ -15,7 +15,29 @@ export default class SrtPreviewer extends Component {
     this.props.makeSrt(filteredArray);
   }
 
-
+  handleDownload(){
+    let output = '';
+    this.props.subtitles.map((subtitle, index) => {
+      output += (index+1) + '\r\n';
+      output += (subtitle.srtStartTimecode) + ' --> ' + (subtitle.srtEndTimecode) + '\r\n';
+      output += (subtitle.text)? subtitle.text : subtitle.defaultTag;
+      output += '\r\n\r\n';
+    });
+    
+    let blob = new Blob([output], {type: 'text/srt'});
+    let filename = '字幕.srt';
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        var elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;
+        document.body.appendChild(elem)
+        elem.click();
+        document.body.removeChild(elem);
+    }
+  }
 
   renderRows(subtitlesArray){
 
@@ -37,7 +59,7 @@ export default class SrtPreviewer extends Component {
   render(){
     return (
       <div>
-        <button className="huge fluid ui button">
+        <button className="huge fluid ui button" onClick={this.handleDownload.bind(this)}>
           <i className="download icon"></i>
           匯出
         </button>
